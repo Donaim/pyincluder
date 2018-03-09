66ef2696ed5960f4d6bc446a46ae84647f70af78
Donaim <0@kek.com>
2018-03-09 19:50:57 +0100

    moved get_arg to helper

--- src/helper.py
+++ src/helper.py
@@ -4,3 +4,27 @@ def format_path(path):
     path = path.replace('/', os.path.sep).replace('\\', os.path.sep)
     if (path[0] == '~' and path[1] == os.path.sep): path = os.path.expanduser('~') + path[1:]
     return path
+
+
+def get_next_token_arg(text, name, name_len, open_char, close_char, ss):
+    copy = text.lstrip()
+    if not copy.startswith(name): return (text, None)
+    
+    copy = copy[name_len:]
+    copy = copy.lstrip()
+    if open_char != None:
+        if copy[0] != open_char: raise Exception("bad include syntax: {} token argument has to begin with '{}' !".format(name, open_char))
+        copy = copy[1:]
+    copy = copy.lstrip(ss)
+    if close_char != None:
+        close_index = copy.find(close_char)
+        if close_index == -1: raise Exception("bad include syntax: {} token argument has to end with '{}' !".format(name, close_char))
+    else:
+        close_index = 0
+        while close_index < len(copy) and not copy[close_index].isspace(): close_index += 1
+    
+    re = copy[:close_index]
+    re = re.rstrip(ss)
+    copy = copy[close_index + 1:]
+    copy = copy.lstrip()
+    return (copy, re)
--- src/include_line.py
+++ src/include_line.py
@@ -1,7 +1,7 @@
 
 import src.config as config
 from src.line import line
-import src.helper
+from src.helper import *
 
 import os
 
@@ -11,28 +11,6 @@ at_key = 'at'
 at_key_len = len(at_key)
 if_key = 'if'
 if_key_len = len(if_key)
-def get_next_token_arg(text, name, name_len, open_char, close_char, ss):
-    copy = text.lstrip()
-    if not copy.startswith(name): return (text, None)
-    
-    copy = copy[name_len:]
-    copy = copy.lstrip()
-    if open_char != None:
-        if copy[0] != open_char: raise Exception("bad include syntax: {} token argument has to begin with '{}' !".format(name, open_char))
-        copy = copy[1:]
-    copy = copy.lstrip(ss)
-    if close_char != None:
-        close_index = copy.find(close_char)
-        if close_index == -1: raise Exception("bad include syntax: {} token argument has to end with '{}' !".format(name, close_char))
-    else:
-        close_index = 0
-        while close_index < len(copy) and not copy[close_index].isspace(): close_index += 1
-    
-    re = copy[:close_index]
-    re = re.rstrip(ss)
-    copy = copy[close_index + 1:]
-    copy = copy.lstrip()
-    return (copy, re)
 
 class in_line(line): # include_line
     def __init__(self, l: line, path: str, in_args: str):
@@ -40,7 +18,7 @@ class in_line(line): # include_line
         self.line.sfile.sc.include_list.append(self)
 
         self.path = path
-        self.realpath = src.helper.format_path(path)
+        self.realpath = format_path(path)
         if not os.path.isabs(self.realpath): self.realpath = os.path.join(self.line.sfile.dirname, path)
         self.target_file = None # gonna get it later
 
